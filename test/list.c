e2e38b5523e2e144cc4d377585884c828cf1dca3
Dongmin Lee <ldm2468@gmail.com>
2022-01-04 05:20:20 +0900

    Improve list foreach test readability

--- test/list.c
+++ test/list.c
@@ -23,25 +23,36 @@ int test_sanity() {
 }
 
 int test_foreach() {
-    int target[] = {1, 0, 2, 0, 5, 3, 5, 4};
-    int j = 0;
     ilist *list = ilist_new();
     ilist_push_back(list, 1);
     ilist_push_back(list, 2);
     ilist_push_back(list, 3);
     ilist_push_back(list, 4);
-    cgs_list_foreach(ilist, list, n, i) {
-        if (i <= 2) {
-            ilist_insert_after(list, n, 0);
-        } else {
-            ilist_insert_before(list, n, 5);
+
+    {
+        int target[] = {1, 2, 3, 4};
+        int j = 0;
+        cgs_list_foreach(ilist, list, n, i) {
+            CNIT_ASSERT(i == target[j++]);
+            if (i <= 2) {
+                ilist_insert_after(list, n, 0);
+            } else {
+                ilist_insert_before(list, n, 5);
+            }
         }
     }
+
     CNIT_ASSERT(list->size == 8);
-    cgs_list_foreach(ilist, list, _, i) {
-        CNIT_ASSERT(i == target[j++]);
+
+    {
+        int target[] = {1, 0, 2, 0, 5, 3, 5, 4};
+        int j = 0;
+        cgs_list_foreach(ilist, list, _, i) {
+            CNIT_ASSERT(i == target[j++]);
+        }
     }
-    ilist_clear(list);
+
+    ilist_free(list);
     return 0;
 }
 
