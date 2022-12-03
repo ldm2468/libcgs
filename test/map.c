ffeba153e4d1089f1367003afcb950a607cb809d
Dongmin Lee <ldm2468@snu.ac.kr>
2022-12-04 08:42:27 +0900

    Add map insert and erase tests

--- test/map.c
+++ test/map.c
@@ -1,25 +1,25 @@
 #include <stdint.h>
 
-#define cgs_key int
-#define cgs_value int
-static inline uint32_t iimap_hash(int key) {
-    return key;
-}
+#define cgs_map_key int
+#define cgs_map_value int
+#define cgs_map_load_factor 50
+#define cgs_map_default_hash
 #define cgs_map_name iimap
 #include "cgs_map.h"
 #define cgs_iimap 1
 
-#define cgs_key char *
-#define cgs_value int
+#define cgs_map_key char *
+#define cgs_map_value int
 #define cgs_map_default_hash_str
 #define cgs_map_name simap
 #include "cgs_map.h"
 #define cgs_simap 1
 
-#define cgs_key long
-#define cgs_value long
-#define cgs_map_default_hash
+#define cgs_map_key int64_t
+#define cgs_map_value int64_t
 #define cgs_map_name llmap
+#define cgs_map_default_hash
+#define cgs_map_default_value (-1)
 #include "cgs_map.h"
 #define cgs_llmap 1
 
@@ -51,7 +51,53 @@ int test_hash() {
     return 0;
 }
 
+int test_map_insert() {
+    iimap *map = iimap_new();
+    for (int i = 0; i < TEST_COUNT; i++) {
+        iimap_insert(map, i * i, i);
+        CNIT_ASSERT(map->size == i + 1);
+    }
+
+    int square = 0;
+    int sqrt = 0;
+    int delta = 1;
+    for (int i = 0; i < TEST_COUNT * TEST_COUNT; i++) {
+        if (i == square) {
+            CNIT_ASSERT(iimap_find(map, i) == sqrt);
+            square += delta;
+            delta += 2;
+            sqrt++;
+        } else {
+            CNIT_ASSERT(iimap_find(map, i) == 0);
+        }
+    }
+    iimap_free(map);
+    return 0;
+}
+
+int test_map_erase() {
+    llmap *map = llmap_new();
+    for (int i = 0; i < TEST_COUNT * 5; i++) {
+        llmap_insert(map, i * 6, i * 6 + 1);
+        CNIT_ASSERT(llmap_find(map, i * 6) == i * 6 + 1);
+    }
+
+    for (int i = 0; i < TEST_COUNT * 2; i++) {
+        CNIT_ASSERT(llmap_erase(map, i * 15) == (i % 2 == 0 ? i * 15 + 1 : -1));
+        CNIT_ASSERT(map->size == TEST_COUNT * 5 - (i / 2) - 1);
+    }
+
+    for (int i = 0; i < TEST_COUNT * 30; i++) {
+        CNIT_ASSERT(llmap_find(map, i) == ((i % 6 == 0 && i % 15 != 0) ? i + 1 : -1));
+    }
+
+    llmap_free(map);
+    return 0;
+}
+
 int main() {
-    cnit_add_test(test_hash, "Test hashing functions");
+    cnit_add_test(test_hash, "Hashing functions");
+    cnit_add_test(test_map_insert, "Map insert/find operations");
+    cnit_add_test(test_map_erase, "Map insert/erase operations");
     return cnit_run_tests();
 }
